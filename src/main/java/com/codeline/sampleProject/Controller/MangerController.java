812fccab918097c269c7920dc8cb5ae86512799b
bu-younes <alg7afri.m@gmail.com>
2023-06-04 11:11:41 +0400

    update Manger Controller

--- src/main/java/com/codeline/sampleProject/Controller/MangerController.java
+++ src/main/java/com/codeline/sampleProject/Controller/MangerController.java
@@ -1,7 +1,53 @@
 package com.codeline.sampleProject.Controller;
 
+import com.codeline.sampleProject.Models.Manager;
+import com.codeline.sampleProject.Service.ManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
+@RequestMapping(value = "manager")
 public class MangerController {
+
+    @Autowired
+    ManagerService managerService;
+
+
+    @RequestMapping("manager/create")
+    @RequestMapping("create")
+    public void saveManager (@RequestBody GetManagerRequestObject managerRequestObject) {
+        createManager(managerRequestObject);
+    }
+
+    @RequestMapping("manager/get")
+    @RequestMapping("get")
+    public List<Manager> getManager () {
+        return managerService.getManager();
+    }
+
+    //Path Variable
+    @RequestMapping("manager/get/{managerId}")
+    @RequestMapping("get/{managerId}")
+    public GetManagerResponse createManager (@PathVariable Long managerId) {
+        return managerService.getManagerById(managerId);
+    }
+    public void createManager(GetManagerRequestObject managerRequestObject) {
+        Manager manager = new Manager();
+        manager.setDepartment(managerRequestObject.getDepartment());
+        manager.setTeamName("The Moon");
+        manager.setCreatedDate(new Date());
+        manager.setIsActive(true);
+        managerService.saveManager(manager);
+    }
+
+    @RequestMapping(value = "findManagerByTeamName", method = RequestMethod.GET)
+    public Manager getManagerByTeamName(@RequestParam String teamName){
+        return managerService.getManagerByTeamName(teamName);
+    }
+
+
 }
