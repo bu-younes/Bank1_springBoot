4aa7b714d75702a79661a8fd74f39f4d5f6ad96d
bu-younes <alg7afri.m@gmail.com>
2023-06-04 11:43:31 +0400

    update ManagerRepository

--- src/main/java/com/codeline/sampleProject/Repository/ManagerRepository.java
+++ src/main/java/com/codeline/sampleProject/Repository/ManagerRepository.java
@@ -4,7 +4,9 @@ import com.codeline.sampleProject.Models.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ManagerRepository  extends JpaRepository<Manager, Long> {
 
     @Query("SELECT * FROM Manager WHERE teamName=:nameOfTeam")
